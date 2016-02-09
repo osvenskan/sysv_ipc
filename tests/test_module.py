dad5e2356767bbd5019d2123a4675b1b16c8ed2f
Philip Semanchuk <philip@PySpoken.com>
2016-02-08 23:01:16 -0500

    removed dead code and uncommented code that I'd temporarily commented out

--- tests/test_module.py
+++ tests/test_module.py
@@ -16,42 +16,31 @@ import base as tests_base
 
 ONE_MILLION = 1000000
 
-# class TestModuleConstants(tests_base.Base):
-#     """Check that the sysv_ipc module-level constants are defined as expected"""
-#     def test_constant_values(self):
-#         """test that constants are what I expect"""
-#         self.assertEqual(sysv_ipc.IPC_CREX, sysv_ipc.IPC_CREAT | sysv_ipc.IPC_EXCL)
-#         self.assertEqual(sysv_ipc.PAGE_SIZE, resource.getpagesize())
+class TestModuleConstants(tests_base.Base):
+    """Check that the sysv_ipc module-level constants are defined as expected"""
+    def test_constant_values(self):
+        """test that constants are what I expect"""
+        self.assertEqual(sysv_ipc.IPC_CREX, sysv_ipc.IPC_CREAT | sysv_ipc.IPC_EXCL)
+        self.assertEqual(sysv_ipc.PAGE_SIZE, resource.getpagesize())
 
-#         self.assertIn(sysv_ipc.SEMAPHORE_TIMEOUT_SUPPORTED, (True, False))
+        self.assertIn(sysv_ipc.SEMAPHORE_TIMEOUT_SUPPORTED, (True, False))
 
-#         self.assertGreaterEqual(sysv_ipc.SEMAPHORE_VALUE_MAX, 1)
+        self.assertGreaterEqual(sysv_ipc.SEMAPHORE_VALUE_MAX, 1)
 
-#         self.assertTrue(isinstance(sysv_ipc.VERSION, str))
+        self.assertTrue(isinstance(sysv_ipc.VERSION, str))
 
-# class TestModuleErrors(tests_base.Base):
-#     """Exercise the exceptions defined by the module"""
-#     def test_errors(self):
-#         self.assertTrue(issubclass(sysv_ipc.Error, Exception))
-#         self.assertTrue(issubclass(sysv_ipc.InternalError, sysv_ipc.Error))
-#         self.assertTrue(issubclass(sysv_ipc.PermissionsError, sysv_ipc.Error))
-#         self.assertTrue(issubclass(sysv_ipc.ExistentialError, sysv_ipc.Error))
-#         self.assertTrue(issubclass(sysv_ipc.BusyError, sysv_ipc.Error))
-#         self.assertTrue(issubclass(sysv_ipc.NotAttachedError, sysv_ipc.Error))
+class TestModuleErrors(tests_base.Base):
+    """Exercise the exceptions defined by the module"""
+    def test_errors(self):
+        self.assertTrue(issubclass(sysv_ipc.Error, Exception))
+        self.assertTrue(issubclass(sysv_ipc.InternalError, sysv_ipc.Error))
+        self.assertTrue(issubclass(sysv_ipc.PermissionsError, sysv_ipc.Error))
+        self.assertTrue(issubclass(sysv_ipc.ExistentialError, sysv_ipc.Error))
+        self.assertTrue(issubclass(sysv_ipc.BusyError, sysv_ipc.Error))
+        self.assertTrue(issubclass(sysv_ipc.NotAttachedError, sysv_ipc.Error))
 
 class TestModuleFunctions(tests_base.Base):
     """Exercise the sysv_ipc module-level functions"""
-    # def test_constant_values(self):
-    #     """test that constants are what I expect"""
-    #     self.assertEqual(sysv_ipc.IPC_CREX, sysv_ipc.IPC_CREAT | sysv_ipc.IPC_EXCL)
-    #     self.assertEqual(sysv_ipc.PAGE_SIZE, resource.getpagesize())
-
-    #     self.assertIn(sysv_ipc.SEMAPHORE_TIMEOUT_SUPPORTED, (True, False))
-
-    #     self.assertGreaterEqual(sysv_ipc.SEMAPHORE_VALUE_MAX, 1)
-
-    #     self.assertTrue(isinstance(sysv_ipc.VERSION, str))
-
     def test_attach(self):
         """Exercise attach()"""
         # Create memory, write something to it, then detach
@@ -116,14 +105,6 @@ class TestModuleFunctions(tests_base.Base):
         mq = sysv_ipc.MessageQueue(None, sysv_ipc.IPC_CREX)
 
         sysv_ipc.remove_message_queue(mq.id)
-
-        # FIXME remove dead code
-        # if sys.platform.startswith('darwin'):
-        #     # The OS X implementation returns errno = 22 (EINVAL, invalid
-        #     # argument) instead of the more specific EIDRM (ID removed).
-        #     expected_error = OSError
-        # else:
-        #     expected_error = sysv_ipc.ExistentialError
 
         with self.assertRaises(sysv_ipc.ExistentialError):
             sysv_ipc.MessageQueue(mq.key)
