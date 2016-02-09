7b97f291c7064639f62c40e032bbd1c685d96061
Philip Semanchuk <philip@PySpoken.com>
2016-02-09 18:11:15 -0500

    add some helpful text to the comment in probe_results.h

--- prober.py
+++ prober.py
@@ -152,10 +152,15 @@ This header file was generated when you ran setup. Once created, the setup
 process won't overwrite it, so you can adjust the values by hand and
 recompile if you need to.
 
+To enable lots of debug output, add this line and re-run setup.py:
+#define SYSV_IPC_DEBUG
+
 To recreate this file, just delete it and re-run setup.py.
 
 KEY_MIN, KEY_MAX and SEMAPHORE_VALUE_MAX are stored internally in longs, so
-you should never #define them to anything larger than LONG_MAX.
+you should never #define them to anything larger than LONG_MAX regardless of
+what your operating system is capable of.
+
 */
 
 """
