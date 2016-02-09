a477967b06d2f3d729b70a89f90ffcabc6b60df1
Philip Semanchuk <philip@PySpoken.com>
2016-02-09 16:43:12 -0500

    PEP8 compliance, and make code Python 2 & 3 compatible

--- ftok_experiment.py
+++ ftok_experiment.py
@@ -1,17 +1,19 @@
-"""This is an experiment to see how often ftok() returns duplicate keys for
-different filenames. 
-"""
-
-
+"""This is an experiment to see how often ftok() returns duplicate keys for different filenames."""
 import sys
 import os
 import sysv_ipc
+
+# Python 2's raw_input() has become input() in Python 3.
+if sys.version_info[0] == 3:
+    input_method = input
+else:
+    input_method = raw_input  # noqa - tell flake8 to chill
 
 if len(sys.argv) == 2:
     start_path = sys.argv[1]
 else:
     msg = "Start path? [Default = your home directory] "
-    start_path = raw_input(msg)
+    start_path = input_method(msg)
     if not start_path:
         start_path = "~"
 
@@ -21,7 +23,7 @@ start_path = os.path.abspath(start_path)
 
 # For every filename in the tree, generate a key and associate the filename
 # with that key via a dictionary.
-d = { }
+d = {}
 nfilenames = 0
 for path, dirnames, filenames in os.walk(start_path):
     for filename in filenames:
@@ -30,12 +32,12 @@ for path, dirnames, filenames in os.walk(start_path):
 
         nfilenames += 1
 
-        #print "Processing %s..." % filename
+        # print("Processing %s..." % filename)
 
         key = sysv_ipc.ftok(filename, 42, True)
 
         if key not in d:
-            d[key] = [ ]
+            d[key] = []
 
         d[key].append(filename)
 
@@ -44,7 +46,6 @@ ndups = 0
 for key in d:
     if len(d[key]) > 1:
         ndups += len(d[key])
-        print key, d[key]
+        print(key, d[key])
 
-print "Out of {0} unique filenames, I found {1} duplicate keys.".format(nfilenames, ndups)
-
+print("Out of {0} unique filenames, I found {1} duplicate keys.".format(nfilenames, ndups))
