578d4084644b9b936bf778f9cc27f04c8d46b1c8
osvenskan <philip@pyspoken.com>
2018-01-31 11:22:58 -0500

    PEP8 fixes, also improve form for try/except, improve messages.

--- demos/message_queues/cleanup.py
+++ demos/message_queues/cleanup.py
@@ -3,15 +3,14 @@ import utils
 
 params = utils.read_params()
 
+key = params["KEY"]
 
 try:
-    mq = sysv_ipc.MessageQueue(params["KEY"])
+    mq = sysv_ipc.MessageQueue(key)
+except sysv_ipc.ExistentialError:
+    print('''Message queue with key "{}" doesn't exist.'''.format(key))
+else:
     mq.remove()
-    s = "message queue %d removed" % params["KEY"]
-    print (s)
-except:
-    print ("message queue doesn't exist")
-    
+    print('Message queue with key "{}" removed'.format(key))
 
-
-print ("\nAll clean!")
+print("\nAll clean!")
